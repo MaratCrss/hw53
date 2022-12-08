1b097387dba24d06a2f30be6c4913adb0e815795
Marat <marat.mashanlo@gmail.com>
2022-12-09 00:00:25 +0600

    dobavil urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 
-from webapp.projects import ProjectsView, CreateProject, ProjectView
-from webapp.views import TasksView, TaskView, CreateTask, UpdateView, DeleteTask, CreateTaskWithProject
+from webapp.projects import ProjectsView, CreateProject, ProjectView, UpdateProject, DeleteProject
+from webapp.views import TasksView, TaskView, CreateTask, UpdateTask, DeleteTask, CreateTaskWithProject
 
 urlpatterns = [
     path('', ProjectsView.as_view(), name='projects'),
     path('tasks/', TasksView.as_view(), name='tasks'),
     path('task_view/<int:pk>/', TaskView.as_view(), name='task_view'),
     path('project_view/<int:pk>/', ProjectView.as_view(), name='project_view'),
-    path('create_task/', CreateTask.as_view(), name='create_task'),
+    path('task/create_task/', CreateTask.as_view(), name='create_task'),
     path('create_task/with_project/<int:pk>/', CreateTaskWithProject.as_view(), name='create_task_with_project'),
-    path('create_project/', CreateProject.as_view(), name='create_project'),
-    path('update_task/<int:pk>/', UpdateView.as_view(), name='update_task'),
-    path('delete_task/<int:pk>/', DeleteTask.as_view(), name='delete_task'),
+    path('project/create_project/', CreateProject.as_view(), name='create_project'),
+    path('task/update_task/<int:pk>/', UpdateTask.as_view(), name='update_task'),
+    path('project/update_project/<int:pk>/', UpdateProject.as_view(), name='update_project'),
+    path('task/delete_task/<int:pk>/', DeleteTask.as_view(), name='delete_task'),
+    path('project/delete_project/<int:pk>/', DeleteProject.as_view(), name='delete_project'),
 ]
