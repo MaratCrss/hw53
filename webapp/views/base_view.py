5e91cbd895f579be8c9a17ad7b8e7e4622ad0c69
Marat <marat.mashanlo@gmail.com>
2022-12-16 13:49:40 +0600

    zamenil na soft delete

--- webapp/views/base_view.py
+++ webapp/views/base_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views import View
 
 
@@ -30,3 +30,39 @@ class FormView(View):
     def form_invalid(self, form):
         context = self.get_context(form=form)
         return render(self.request, self.template_name, context)
+
+
+class SoftDelete(View):
+    template_name = None
+    confirm_deletion = True
+    model = None
+    key_kwarg = 'pk'
+    context_key = 'object'
+    redirect_url = ''
+    object = None
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.confirm_deletion:
+            return render(request, self.template_name, self.get_context_data())
+        else:
+            self.perform_delete()
+            return redirect(self.get_redirect_url())
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.perform_delete()
+        return redirect(self.get_redirect_url())
+
+    def perform_delete(self):
+        self.object.delete()
+
+    def get_context_data(self, **kwargs):
+        return {self.context_key: self.object}
+
+    def get_object(self):
+        pk = self.kwargs.get(self.key_kwarg)
+        return get_object_or_404(self.model, pk=pk)
+
+    def get_redirect_url(self):
+        return self.redirect_url
